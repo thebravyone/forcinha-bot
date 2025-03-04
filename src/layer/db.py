b64616df2e5241a55c3063e762c742223e82bac1
Guilherme Serradilha <thebravyone@gmail.com>
2025-03-04 16:26:14 -0300

    feat: add batch get

--- src/layer/db.py
+++ src/layer/db.py
@@ -13,7 +13,6 @@ user_table = db.Table("forcinha_user")
 
 
 class state_token:
-    @staticmethod
     def add(discord_user_id: int) -> str:
         token = generate(size=16)
         state_token_table.put_item(
@@ -27,15 +26,13 @@ class state_token:
         )
         return token
 
-    @staticmethod
     def get(token: str) -> dict:
         response = state_token_table.get_item(Key={"state_token": token})
         return response.get("Item", None)
 
 
 class user:
-    @staticmethod
-    def add(discord_user_id: int, character_id: int):
+    def add(discord_user_id: str, character_id: str):
         user_table.put_item(
             Item={
                 "discord_user_id": discord_user_id,
@@ -44,7 +41,19 @@ class user:
         )
         return
 
-    @staticmethod
-    def get(discord_user_id: int):
+    def get(discord_user_id: str):
         response = user_table.get_item(Key={"discord_user_id": discord_user_id})
         return response.get("Item", None)
+
+    def get_many(discord_user_ids: list[str]):
+        keys = [{"discord_user_id": id} for id in discord_user_ids]
+
+        keys_batch = [keys[i : i + 100] for i in range(0, len(keys), 100)]
+
+        items = []
+        for keys in keys_batch:
+            response = user_table.batch_get_item(Keys=keys)
+            items.extend(response.get("Items", []))
+
+        response = user_table.batch_get_item(Keys=keys)
+        return response.get("Items", None)
