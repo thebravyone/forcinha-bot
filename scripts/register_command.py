d82088be02dab7e8c0cf3f3df60c9aaecc55afce
Guilherme Serradilha <thebravyone@gmail.com>
2025-03-04 01:18:09 -0300

    build: add script to register Discord commands

---
+++ scripts/register_command.py
@@ -0,0 +1,36 @@
+import json
+import os
+
+import httpx
+
+APP_ID = os.environ.get("APP_ID", None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+
+url = f"https://discord.com/api/v10/applications/{APP_ID}/commands"
+headers = {"Authorization": f"Bot {BOT_TOKEN}"}
+
+commands = [
+    {
+        "name": "vincular",
+        "type": 1,
+        "description": "Vincule sua conta do EVE-Online ao Discord",
+    },
+]
+
+
+def main():
+    for command in commands:
+        response = httpx.post(url, headers=headers, json=command)
+        print(response.json())
+    return
+
+
+def delete_command(command_id: str):
+    url = f"https://discord.com/api/v10/applications/{APP_ID}/commands/{command_id}"
+    httpx.delete(url, headers=headers)
+    return
+
+
+if __name__ == "__main__":
+    main()
+    pass
